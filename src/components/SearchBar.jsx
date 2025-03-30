691587adceab05f29881eac95d883576d2b6f7bb
LIN FUNGULA <LINDA.FUNGULA@GMAIL.COM>
2025-03-30 17:25:09 +0200

    Set Includes Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,22 +4,27 @@ import { data } from "../utils/data";
 
 export const Searchbar = () => {
   const [value, setValue] = useState("");
-  console.log(value);
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
+  const mapData = JSON.stringify(
+    data.hits.map((element) => element.recipe.label)
+  );
+  const includeData = mapData.includes(value);
+  console.log(includeData);
 
   return (
     <div>
       <Input
         m="4"
-        w="90vw"
+        w="75vw"
         variant="filled"
         type="text"
-        value={value}
         placeholder="Search your fooditem by name here"
         onChange={handleChange}
+        shadow="dark-lg"
+        value={value}
       />
     </div>
   );
