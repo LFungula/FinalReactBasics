ee44551b0effbf7d6ee38399a7e22147da2ea041
LIN FUNGULA <LINDA.FUNGULA@GMAIL.COM>
2025-03-30 17:25:18 +0200

    Set Includes Search

--- src/pages/RecipeItemPage.jsx
+++ src/pages/RecipeItemPage.jsx
@@ -1,13 +1,4 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Stack,
-  HStack,
-  Text,
-  Tag,
-  Button,
-} from "@chakra-ui/react";
+import { Flex, Heading, HStack, Text, Tag, Button } from "@chakra-ui/react";
 import { RecipeCard } from "../components/RecipeCard";
 
 export const RecipeItemPage = ({ recipeItem, clickFn }) => {
@@ -20,18 +11,18 @@ export const RecipeItemPage = ({ recipeItem, clickFn }) => {
       wrap="wrap"
       justify="space-evenly"
       w="100vw"
+      minH="100vh"
+      align="flex-start"
     >
-      <Box w="100vw">
-        <Flex justify="center">
-          <Button shadow="dark-lg" onClick={() => clickFn()}>
-            Return to Menu
-          </Button>
-        </Flex>
-      </Box>
-      <Box>
-        <RecipeCard recipeItem={recipeItem} />
-      </Box>
-      <Box
+      <Flex justify="center" w="100vw">
+        <Button shadow="dark-lg" onClick={() => clickFn()}>
+          Return to Menu
+        </Button>
+      </Flex>
+      <RecipeCard recipeItem={recipeItem} />
+      <Flex
+        wrap="wrap"
+        direction="row"
         border="2px dotted seashell"
         p={4}
         m={4}
@@ -39,20 +30,93 @@ export const RecipeItemPage = ({ recipeItem, clickFn }) => {
         borderRadius="lg"
         bgColor="blue.100"
         shadow="dark-lg"
+        justify="center"
       >
-        <Flex wrap="wrap" direction="row">
-          <Heading>
-            <HStack p={1} wrap="wrap" gap={1} justify="center">
-              {recipeItem.healthLabels.map((healthLabel) => (
-                <Tag w="fit-content" key={healthLabel} colorScheme="teal">
-                  {healthLabel}
-                </Tag>
-              ))}
-            </HStack>
-          </Heading>
-          <Text>{recipeItem.label}</Text>
-        </Flex>
-      </Box>
+        <Heading m={2} textAlign="center">
+          Health = Welth
+        </Heading>
+        <HStack p={1} wrap="wrap" gap={1} justify="center">
+          {recipeItem.healthLabels.map((healthLabel) => (
+            <Tag w="fit-content" key={healthLabel} colorScheme="teal">
+              {healthLabel}
+            </Tag>
+          ))}
+        </HStack>
+      </Flex>
+      <Flex
+        wrap="wrap"
+        direction="row"
+        border="2px dotted seashell"
+        p={4}
+        m={4}
+        w="sm"
+        borderRadius="lg"
+        bgColor="teal.100"
+        shadow="dark-lg"
+        justify="center"
+      >
+        <Heading m={2} textAlign="center">
+          What&apos;s in in for me?
+        </Heading>
+        <HStack p={1} wrap="wrap" gap={1} justify="center">
+          {recipeItem.ingredientLines.map((ingredientLine) => (
+            <Tag w="fit-content" key={ingredientLine} colorScheme="blue">
+              {ingredientLine}
+            </Tag>
+          ))}
+        </HStack>
+      </Flex>
+      <Flex
+        wrap="wrap"
+        direction="row"
+        border="2px dotted seashell"
+        p={4}
+        m={4}
+        w="sm"
+        borderRadius="lg"
+        bgColor="lightyellow"
+        shadow="dark-lg"
+        justify="center"
+      >
+        <Heading m={2} textAlign="center">
+          Things you might like to know
+        </Heading>
+        <HStack p={1} wrap="wrap" gap={1} justify="center">
+          <Tag w="fit-content" colorScheme="orange">
+            It takes your chef {recipeItem.totalTime} minutes tp prepare this
+            dish!
+          </Tag>
+          <Tag w="fit-content" colorScheme="orange">
+            Cuisinetype :
+            {recipeItem.cuisineType.map((cuisine) => {
+              return ` ${cuisine},`;
+            })}
+          </Tag>
+        </HStack>
+      </Flex>
+      <Flex
+        wrap="wrap"
+        direction="row"
+        border="2px dotted seashell"
+        p={4}
+        m={4}
+        w="sm"
+        borderRadius="lg"
+        bgColor="cyan.100"
+        shadow="dark-lg"
+        justify="center"
+      >
+        <Heading m={2} textAlign="center">
+          Things you do not like to know
+        </Heading>
+        <HStack p={1} wrap="wrap" gap={1} justify="center">
+          {recipeItem.ingredientLines.map((ingredientLine) => (
+            <Tag w="fit-content" key={ingredientLine} colorScheme="pink">
+              {ingredientLine}
+            </Tag>
+          ))}
+        </HStack>
+      </Flex>
     </Flex>
   );
 };
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,23 +1,26 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import { RecipeCard } from "../components/RecipeCard";
 import { data } from "../utils/data";
+import { Searchbar } from "../components/SearchBar";
 
 export const RecipeListPage = ({ clickFn }) => {
   return (
-    <Flex
-      direction="row"
-      bgColor="blue.100"
-      gap={4}
-      flexWrap="wrap"
-      justifyContent="center"
-    >
-      {data.hits.map((recipeItem) => (
-        <RecipeCard
-          key={recipeItem.recipe.label}
-          recipeItem={recipeItem.recipe}
-          clickFn={clickFn}
-        />
-      ))}
+    <Flex bgColor="blue.100" direction="column">
+      <Flex justify="center" p={4}>
+        <Heading>Welcome to our restaurant!</Heading>
+      </Flex>
+      <Flex justify="center">
+        <Searchbar />
+      </Flex>
+      <Flex direction="row" gap={4} flexWrap="wrap" justifyContent="center">
+        {data.hits.map((recipeItem) => (
+          <RecipeCard
+            key={recipeItem.recipe.label}
+            recipeItem={recipeItem.recipe}
+            clickFn={clickFn}
+          />
+        ))}
+      </Flex>
     </Flex>
   );
 };
